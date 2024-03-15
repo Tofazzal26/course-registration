61b28e87331942fba818cf356f51f3cbbc3dadff
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-15 16:53:00 +0600

    creadit validation finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import Courses from "./Components/Courses/Courses";
 import Header from "./Components/Header/Header";
 import SelectedCart from "./Components/SelectedCart/SelectedCart";
 
+export const totalCredit = 15;
+
 function App() {
   const [cart, setCart] = useState([]);
 
   const handleCart = (course) => {
+    const isExisted = cart.find((one) => one.id == course.id);
+
+    const creditValue = cart.reduce((a, b) => a + b.credit, 0);
+
+    if (creditValue + course.credit > totalCredit) {
+      return alert("15 over");
+    }
+
     const newCart = [...cart, course];
-    setCart(newCart);
+    if (!isExisted) {
+      setCart(newCart);
+    }
   };
 
   return (
--- src/Components/SelectedCart/SelectedCart.jsx
+++ src/Components/SelectedCart/SelectedCart.jsx
@@ -8,8 +8,19 @@ const SelectedCart = ({ cart }) => {
           </h2>
           <div className="divider"></div>
           <h2 className="text-lg font-semibold">Course Name</h2>
+          <div>
+            <ul>
+              {cart.map((nam, ind) => (
+                <li key={nam.id}>
+                  {ind + 1} {nam.name}
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className="divider"></div>
-          <h2 className="text-[#585858]">Total Credit Hour : 13</h2>
+          <h2 className="text-[#585858]">
+            Total Credit Hour : {cart.reduce((a, b) => a + b.credit, 0)}
+          </h2>
           <div className="divider"></div>
           <h2 className="text-[#585858]">
             Total Price : {cart.reduce((b, a) => b + a.price, 0)} USD
